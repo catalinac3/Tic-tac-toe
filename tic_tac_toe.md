64557d0b3d8baf52026eb6d3f9ae1a68a06b725b
catalina <cdianacatalina@gmail.com>
2020-06-14 19:58:47 +0200

    fixed docs description in the code and completed .md file

--- tic_tac_toe.md
+++ tic_tac_toe.md
@@ -1,10 +1,15 @@
 # tic_tac_toe.py
 
 This file displays the code of the tic-tac-toe written in python. No modules were imported.
-
-The interface with the users is in the terminal. In the terminal both players can make their move, see the board game and an additional board indicating position available were players can do the next move.
-
-The board game and the boar indicating available moves look like this:
-
+ 
+The interface with the users is located in the terminal. In the terminal, both players can make their move and see the development of the game on the board game. An additional board indicating available positions is given as a guide for the players to make the next move. This is done with numbers from 1 to 9
+ 
+The board game and the board indicating available moves look like this:
+ 
 ![tic_tac_toe terminal interface](https://github.com/catalinac3/Projects-in-python/blob/master/images/tic_tac_toe.JPG?raw=true)
-
+ 
+One player marks in the board with an X the other one with O. Where there are 3 in a row the of the same mark, the player related to that mark wins.
+ 
+![tic_tac_toe terminal interface](https://github.com/catalinac3/Projects-in-python/blob/master/images/show_win.JPG?raw=true)
+ 
+In this code, the board is set up as a list of lists in python, or a matrix, not as a list of nine like it is usually made. This brings a bit of an extra challenge on checking whether a player has won or not. Great exercise for python beginners!. Try to write the code of each function by copying down the name of the functions needed with the description of what they should do, to recreate this exercise.
--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -2,8 +2,9 @@
 
 
 def create_a_blank_board():
-    '''This function returns a empty list of 3 of list with 3 position
-    [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
+    ''' Creates a list of 3 lists, each of them with 3 empty positions
+    Returns (list): 
+        [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
     '''
     list_of_three = []
     for i in range(3):
@@ -14,20 +15,28 @@ def create_a_blank_board():
 
 
 def display_board(board):
-    '''This function takes a list of list representing the values of the board 
-    and displays them as a tic tac toe human friendly board by printing it'''
-    line_counter = 0
+    ''' Takes a list of 3 list and displays it in the terminal 
+    as a tic tac toe board using print()
+
+    Parameter:
+    board (list): list of 3 lists with 3 items each
+    '''
+    This function
     for i, line in enumerate(board):
         print(' '+' | '.join(line))
         if i < 2:
             print('---+---+---')
-        line_counter += 1
+    print()
 
 
 def position_indicator_initial():
-    '''This function uses the create_a_blank_board and returns the initial 
-    position map as a list of list, so that the user knows how to select a 
-    position in the board'''
+    '''Uses the create_a_blank_board() funtion and creates the position board
+    so that the user knows how to make a move, this by chossing a number between
+    1 to 9 
+
+    Returns (list): initial position board as a list of list with numbers from 
+    1-9 
+    '''
     board = create_a_blank_board()
     position = 1
     for i in range(3):
@@ -37,21 +46,60 @@ def position_indicator_initial():
     return board
 
 
-# This function doesn't have a return because it is passed as a reference
-# it will be then modified anyways, the same happens with register_move()
-# function bellow
-def register_position_move(position_map, move):
-    '''This function updates the list of list position map, with the move of a 
-    player, so that the user knows which positions are still selectable on the 
-    board'''
+def check_input(move, position_board):
+    ''' Checks if the input of the player for his move is valid or invalid:
+     - is it a number between 1-9 ?
+     - is it a number, that is available in the position board ? 
+
+    Parameters:
+    move(int):
+    position_board(list):
+
+    Returns:
+    (bool):if the input is valid the function returns True, otherwise it 
+    returns False
+    '''
+    try:
+        int(move)
+    except:
+        return False
+
+    if 0 < int(move) < 10:
+        i = (int(move)-1) // 3
+        if move in position_board[i]:
+            return True
+    return False
+
+
+# The following functions: register_position_move() and the register_move(),
+# don't need a return value because the list of list is passed as a reference.
+# It will be modified anyway, by default in python.
+
+def register_position_move(position_board, move):
+    '''Updates the list of list position board, with the move of a 
+    player, by making this position empty and unavailabLe for future moves 
+    during the game.
+
+    Parameters:
+    position_board (list): list of 3 lists with 3 items each
+    move (int): number between 1-9
+    '''
     move = int(move)-1
     i = int(move) // 3
     j = int(move) % 3
-    position_map[i][j] = ' '
+    position_board[i][j] = ' '
 
 
 def register_move(board, move, current_player):
-    '''This function updates the game board, adding the move of a player'''
+    '''Updates the game board, adding the move of a player, X for the x_player 
+    and O for the o_player
+
+    Parameters:
+    board(list)
+    move(int)
+    current_player(str): 'player_X' or 'player O'
+    '''
+
     move = int(move)-1
     i = int(move) // 3
     j = int(move) % 3
@@ -61,23 +109,17 @@ def register_move(board, move, current_player):
         board[i][j] = 'O'
 
 
-def check_input(move, position_map):
-    ''' This funcion checks if the move given by the player is valid, if it is a 
-    valid number between 0-9 and if the number is available in the position map, 
-    and if he input is a number, it returns true for valid or false for invalid '''
-    try:
-        int(move)
-    except:
-        return False
-
-    if 0 < int(move) < 10:
-        i = (int(move)-1) // 3
-        if move in position_map[i]:
-            return True
-    return False
-
-
 def check_if_player_won(board, current_player):
+    '''Checks if the current_player won, does he has 3 in a row
+    horizontally, vertically or diagonally.
+
+    Parameters:
+    current_player(str)
+    board(list)
+
+    Returns:
+    (bool): True if the player has won, False if he hasn't
+     '''
     if current_player == players[0]:
         sign = 'X'
     else:
@@ -114,8 +156,13 @@ def check_if_player_won(board, current_player):
 
 
 def change_player(players, current_player):
-    '''This function takes the list of players and the current player and 
-    change the current_player, so both players switch in each turn'''
+    '''change the current_player to the other player, it determines how's turn 
+    is it
+
+    Parameters:
+    players(list): contains two string items, each one refering to a player
+    current_player(str)
+    '''
     if current_player == players[0]:
         current_player = players[1]
     else:
@@ -129,12 +176,12 @@ board = create_a_blank_board()
 display_board(board)
 
 
-print('\n'+'Make your move by chossing a number on this board')
+print('Make your move by chossing a number on this board')
 # Board indicating initially available positions of the board
 
 # To make a second board to trace position, I have to create a second blank!
-position_map = position_indicator_initial()
-display_board(position_map)
+position_board = position_indicator_initial()
+display_board(position_board)
 
 players = ['player X', 'player O']
 
@@ -152,7 +199,7 @@ while player_won == False and turns < 9:
     while move == 'not accepted':
         move_player = input(
             f'\n{current_player}! Where do you want to make your next move: ')
-        if check_input(move_player, position_map) == True:
+        if check_input(move_player, position_board) == True:
             move = 'accepted'
 
     # insert a X or  O in the position
@@ -160,8 +207,8 @@ while player_won == False and turns < 9:
     display_board(board)
 
     # update position map, with available positions
-    register_position_move(position_map, move_player)
-    display_board(position_map)
+    register_position_move(position_board, move_player)
+    display_board(position_board)
     turns += 1
 
     # check if player has won
